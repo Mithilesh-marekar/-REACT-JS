d45fd6be5aad020559772241191d8d058de49d49
Mithilesh <mithileshmarekar01@gmail.com>
2023-10-17 11:05:41 +0530

    Started with REACT

---
+++ README.md
@@ -0,0 +1,19 @@
+## Learning REACT JS
+
+
+**Install React**
+npx create-react-app project_name
+
+* Got to project Directory
+    cd project_name
+
+* To start Project
+    npm start
+
+* React Runs on Default Port no : 3000
+
+    You can now view learning_react in the browser.
+            Local:http://localhost:3000
+
+
+            
---
+++ learning_react/src/App.js
@@ -0,0 +1,13 @@
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello React | Learning REACT </h1>
+      <p>Manipulated the default files and Code. Deleted some code. Added some new Code. And Run my First React Code.
+      <b>webpack compiled successfully.</b>
+      </p>
+    </div>
+  );
+}
+
+export default App;
