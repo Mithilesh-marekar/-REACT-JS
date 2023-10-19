b84f59f1c2916adefef74e82098ee1882a5037d0
Mithilesh <mithileshmarekar01@gmail.com>
2023-10-19 14:01:59 +0530

    done

---
+++ 01_learning_react/src/App.js
@@ -0,0 +1,15 @@
+
+function App() {
+
+  const Username = "Roman "
+
+
+  return (
+    <div className="App">
+      <h1>Hello React | {Username} </h1>
+      
+    </div>
+  );
+}
+
+export default App;
