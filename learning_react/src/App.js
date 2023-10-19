66efa91e7c26e5d5f961db913c79e401be3ce8b4
Mithilesh <mithileshmarekar01@gmail.com>
2023-10-19 13:48:00 +0530

    Learnt some basic React concepts . manipulated some code. learnt about components , evaluated expressions and fragments

--- learning_react/src/App.js
+++ learning_react/src/App.js
@@ -1,11 +1,13 @@
 
 function App() {
+
+  const Username = "Roman "
+
+
   return (
     <div className="App">
-      <h1>Hello React | Learning REACT </h1>
-      <p>Manipulated the default files and Code. Deleted some code. Added some new Code. And Run my First React Code.
-      <b>webpack compiled successfully.</b>
-      </p>
+      <h1>Hello React | {Username} </h1>
+      
     </div>
   );
 }
